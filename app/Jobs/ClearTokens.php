1431138430f1c1ff15c90499cce5d75af546e3e4
Michael Altufiev <arutomi@gmail.com>
2021-12-21 14:25:38 +0300

    First Queue

---
+++ app/Jobs/ClearTokens.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Laravel\Sanctum\Sanctum;
+
+class ClearTokens implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(
+        protected User $user,
+        protected string $tokenName,
+    ) {
+    }
+
+    public function handle(): void
+    {
+        $userId = $this->user->user_id;
+
+        /** @var Model $tokenModel */
+        $tokenModel = Sanctum::$personalAccessTokenModel;
+        $tokenId = $tokenModel::query()
+            ->where('tokenable_id', '=', $userId)
+            ->where('name', '=', $this->tokenName)
+            ->latest()
+            ->first()
+            ?->id;
+
+        if ($tokenId === null) {
+            return;
+        }
+
+        $tokenModel::query()
+            ->where('tokenable_id', '=', $userId)
+            ->where('name', '=', $this->tokenName)
+            ->whereKeyNot($tokenId)
+            ->delete();
+    }
+}
